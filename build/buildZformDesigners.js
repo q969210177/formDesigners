a0ed38bc5c319a2c353b888b78dda74e0bbba86e
q969210177 <969210177@qq.com>
2022-06-29 13:54:59 +0800

    回头测试一下打包

--- build/buildZformDesigners.js
+++ build/buildZformDesigners.js
@@ -71,17 +71,17 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // 将 JS 字符串生成为 style 节点
-          "style-loader",
-          // 将 CSS 转化成 CommonJS 模块
-          "css-loader",
-          // 将 Sass 编译成 CSS
-          "sass-loader",
-        ],
-      },
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // 将 JS 字符串生成为 style 节点
+      //     "style-loader",
+      //     // 将 CSS 转化成 CommonJS 模块
+      //     "css-loader",
+      //     // 将 Sass 编译成 CSS
+      //     "sass-loader",
+      //   ],
+      // },
     ],
   },
   plugins: [

3c917e28593ec8423f2fb08385e3acbe4f80f22a
q969210177 <969210177@qq.com>
2022-07-05 12:14:43 +0800

    写完文件执行脚本还是有问题

--- build/autoZformcreate.js
+++ build/autoZformcreate.js
@@ -6,69 +6,57 @@ const page = require("../package.json");
 
 const newVersion = page.dependencies.zformcreate.split(".");
 newVersion[2] = Number(newVersion[2]) + 1 + "";
-// if (newVersion[2] === 9) {
-//   newVersion[1] = Number(newVersion[1]) + 1 + "";
-// } else if (newVersion[1] === 9) {
-//   newVersion[0] = Number(newVersion[0]) + 1 + "";
-// } else {
-//   newVersion[2] = Number(newVersion[2]) + 1 + "";
-// }
-
-// const newPackage = {
-//   name: "zformcreate",
-//   version:
-//   description: "根据特地规则生成表单",
-//   main: "index.js",
-//   files: ["index.js"],
-//   typings: "index.d.ts",
-//   keywords: ["form", "vue", "antd"],
-//   license: "MIT",
-//   peerDependencies: {
-//     vue: "^2.5.17",
-//     "ant-design-vue": "^1.7.8",
-//     "core-js": "^3.6.5",
-//   },
-// };
-fs.writeFile(
-  path.resolve(__dirname, "../lib/zformcreate/package.json"),
-  `
-  {
-    "name":"zformcreate",
-    "version":"${newVersion.join(".")}",
-    "description":"根据特地规则生成表单",
-    "main":"index.js",
-    "files":["index.js","index.d.ts"],
-    "typings":"index.d.ts",
-    "keywords":["form","vue","antd"],
-    "license":"MIT",
-    "peerDependencies":{
-        "vue":"^2.5.17",
-        "ant-design-vue":"^1.7.8",
-        "core-js":"^3.6.5"
+async function writeFilePackage() {
+  await fs.writeFile(
+    path.resolve(__dirname, "../lib/zformcreate/package.json"),
+    `
+    {
+      "name":"zformcreate",
+      "version":"${newVersion.join(".")}",
+      "description":"根据特地规则生成表单",
+      "main":"index.js",
+      "files":["index.js","index.d.ts"],
+      "typings":"index.d.ts",
+      "keywords":["form","vue","antd"],
+      "license":"MIT",
+      "peerDependencies":{
+          "vue":"^2.5.17",
+          "ant-design-vue":"^1.7.8",
+          "core-js":"^3.6.5"
+      }
     }
-  }
-  `,
-  () => {
-    console.log("运行完毕");
-  }
-);
-
-fs.writeFile(
-  path.resolve(__dirname, "../lib/zformcreate/index.d.ts"),
-  `
+    `,
+    async () => {
+      await console.log("运行完毕 writeFilePackage");
+    }
+  );
+}
+async function writeFileType() {
+  await fs.writeFile(
+    path.resolve(__dirname, "../lib/zformcreate/index.d.ts"),
+    `
   import Vue, { VNode } from "vue";
   export declare class zformcreate extends Vue {
     static install(vue: typeof Vue): void;
     static components(components: VNode): void;
   }
   `,
-  () => {
-    console.log("运行完毕");
-  }
-);
-// shell.exec(`
-//   cd ./lib/zformcreate/;
-//   ls -l;
-//   // npm publish
-//   `);
+    async () => {
+      await console.log("运行完毕 writeFileType");
+      shell.exec(`
+      cd ./lib/zformcreate/;
+      npm publish
+      cd ../
+      cd ../
+      ls -l;
+      `);
+    }
+  );
+}
+async function main() {
+  await writeFilePackage();
+  await writeFileType();
+}
+main();
+
 // shell.exec(`npm publish`);

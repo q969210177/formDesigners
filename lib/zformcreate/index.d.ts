5205fe34840bc8b3981b6c29108151f085a65ee6
q969210177 <969210177@qq.com>
2022-07-14 11:24:33 +0800

    提交一下 打包后的内容

---
+++ lib/zformcreate/index.d.ts
@@ -0,0 +1,7 @@
+
+  import Vue, { VNode } from "vue";
+  export declare class zformcreate extends Vue {
+    static install(vue: typeof Vue): void;
+    static components(components: VNode): void;
+  }
+  
